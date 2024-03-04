98063e48ec75c954d053a62a7eb96497b118dce0
Gabriel 'Kazz' Morgado <kazzxd1@gmail.com>
2024-03-04 04:27:31 -0300

    ORM Model

--- .kernel/Flames/Model.php
+++ .kernel/Flames/Model.php
@@ -5,16 +5,51 @@ namespace Flames;
 use Flames\Collection\Arr;
 use Flames\ORM\Database;
 use Flames\ORM\Table;
+use JetBrains\PhpStorm\NoReturn;
 
 abstract class Model
 {
-    private const __VERSION__ = 1;
+    private const __VERSION__ = 5;
 
-    private static string|null $database = null;
-    private static string|null $table    = null;
+    private static bool $__setup = false;
+    private static string $database;
+    private static string $table;
+    private static Arr $column;
+
+    public static function getTable() : string
+    {
+        return self::$table;
+    }
+
+    public static function setTable(string $table) : bool
+    {
+        if (empty($table) === true)
+            return false;
+
+        self::$table = $table;
+        return true;
+    }
+
+    public static function getDatabase() : string
+    {
+        return self::$database;
+    }
+
+    public static function setDatabase(string $database) : bool
+    {
+        if (empty($database) === true)
+            return false;
+
+        self::$database = $database;
+        return true;
+    }
 
     public static function __constructStatic()
     {
+        if (self::$__setup === true) {
+            return;
+        }
+
         $class = static::class;
         $path = (ROOT_PATH . str_replace('\\', '/', $class) . '.php');
 
@@ -28,7 +63,6 @@ abstract class Model
                 self::__setup($data);
                 return;
             }
-            return;
         }
 
         $data = self::__getReflection($class);
@@ -42,23 +76,127 @@ abstract class Model
         @touch($cachePath, $currentTime);
 
         self::__setup($data);
+        self::$__setup = true;
     }
 
     private static function __getReflection(string $class) : Arr
     {
         $data = Arr([
-            'version' => self::__VERSION__,
-            'class'   => Arr()
+            'version'  => self::__VERSION__,
+            'database' => null,
+            'table'    => null,
+            'column'   => Arr()
         ]);
 
         $reflection = new \ReflectionClass($class);
+
+        // Get database/table
         $attributes = $reflection->getAttributes();
         foreach($attributes as $attribute) {
-            $data->class[] = Arr([
-                'name'      => $attribute->getName(),
-                'arguments' => Arr($attribute->getArguments()),
-                'target'    => $attribute->getTarget(),
+            $attributeName = $attribute->getName();
+
+            if ($attributeName === \Flames\ORM\Database::class) {
+                $arguments = $attribute->getArguments();
+                if (isset($arguments['name']))
+                    $data->database = $arguments['name'];
+            }
+
+            elseif ($attributeName === \Flames\ORM\Table::class) {
+                $arguments = $attribute->getArguments();
+                if (isset($arguments['name']))
+                    $data->table = $arguments['name'];
+            }
+        }
+        if ($data->table === null) {
+            $data->table = str_replace('\\', '_', strtolower(substr($class, 17)));
+        }
+
+        // Get columns
+        $properties = $reflection->getProperties();
+        foreach($properties as $property) {
+            $columnProperty = $property->getName();
+            if (str_starts_with($columnProperty, '_') === true) {
+                continue;
+            }
+
+            $column = Arr([
+                'property'      => $columnProperty,
+                'name'          => null,
+                'type'          => null,
+                'size'          => null,
+                'nullable'      => false,
+                'default'       => $property->getDefaultValue(),
+                'primary'       => false,
+                'index'         => false,
+                'unique'        => false,
+                'autoIncrement' => false,
             ]);
+
+            $type = $property->getType();
+
+            if ($type instanceof \ReflectionUnionType) {
+                $types = $type->getTypes();
+
+                $foundType = false;
+                foreach ($types as $type) {
+                    $typeName = $type->getName();
+
+                    if ($typeName === 'null') {
+                        $column->nullable = true;
+                    } elseif ($foundType === false) {
+                        $column->type = $typeName;
+                        $foundType = true;
+                    }
+                }
+            } else {
+                $column->nullable = $type->allowsNull();
+                $column->type     = $type->getName();
+            }
+
+            $attributes = $property->getAttributes();
+            foreach($attributes as $attribute) {
+                if ($attribute->getName() === ORM\Column::class) {
+
+                    $arguments = $attribute->getArguments();
+
+                    if (isset($arguments['nullable']) === true) {
+                        $column->nullable = $arguments['nullable'];
+                    }
+                    if (isset($arguments['type']) === true) {
+                        $column->type = $arguments['type'];
+                    }
+                    if (isset($arguments['length']) === true) {
+                        $column->size = $arguments['length'];
+                    }
+                    if (isset($arguments['default']) === true) {
+                        $column->default = $arguments['default'];
+                    }
+                    if (isset($arguments['name']) === true) {
+                        $column->name = $arguments['name'];
+                    }
+                    if (isset($arguments['index']) === true) {
+                        $column->index = $arguments['index'];
+                    }
+                    if (isset($arguments['primary']) === true) {
+                        $column->primary = $arguments['primary'];
+                    }
+                    if (isset($arguments['index']) === true) {
+                        $column->index = $arguments['index'];
+                    }
+                    if (isset($arguments['autoIncrement']) === true) {
+                        $column->autoIncrement = $arguments['autoIncrement'];
+                    }
+                    if (isset($arguments['unique']) === true) {
+                        $column->unique = $arguments['unique'];
+                    }
+                }
+            }
+
+            if ($column->name === null) {
+                $column->name = $column->property;
+            }
+
+            $data->column[$column->property] = $column;
         }
 
         return $data;
@@ -66,45 +204,48 @@ abstract class Model
 
     private static function __setup(Arr $data)
     {
-        foreach ($data->class as $attribute) {
-            if ($attribute->name === Database::class) {
-                if (isset($attribute->arguments->name) === true) {
-                    self::$database = $attribute->arguments->name;
-                }
-            }
-            elseif ($attribute->name === Table::class) {
-                if (isset($attribute->arguments->name) === true) {
-                    self::$table = $attribute->arguments->name;
-                }
+        self::$database = $data->database;
+        self::$table    = $data->table;
+
+        if ($data->column->length === 0) {
+            throw new \Exception('Model need at least one column.');
+        }
+
+        self::$column = $data->column;
+    }
+
+    #[NoReturn]
+    public function __construct(Arr|array|null $data = null)
+    {
+        if ($data instanceof Arr) {
+            $data = (array)$data;
+        }
+
+        if (is_array($data) === true) {
+            foreach ($data as $key => $value) {
+                $this->__set($key, $value);
             }
         }
     }
 
-    protected static function getTable() : string
+    public function __set(string $key, mixed $value)
     {
-        return self::$table;
+        if (isset(self::$column[$key]) === true) {
+            $this->{$key} = self::__parse(self::$column[$key], $value);
+        }
     }
 
-    protected static function setTable(string $table) : bool
+    public function __get(string $key)
     {
-        if (empty($table) === true)
-            return false;
+        if (isset($this->{$key}) === true)
+            return $this->{$key};
 
-        self::$table = $table;
-        return true;
+        return null;
     }
 
-    protected static function getDatabase() : string
+    private static function __parse(Arr $column, mixed $value = null) : mixed
     {
-        return self::$database;
-    }
-
-    protected static function setDatabase(string $database) : bool
-    {
-        if (empty($database) === true)
-            return false;
-
-        self::$database = $database;
-        return true;
+        // TODO: parse data
+        return $value;
     }
 }
--- App/Server/Model/User.php
+++ App/Server/Model/User.php
@@ -2,17 +2,28 @@
 
 namespace App\Server\Model;
 
+use Flames\Collection\Arr;
 use Flames\Model;
 use Flames\ORM;
+use JetBrains\PhpStorm\NoReturn;
+
+/**
+ * Description for the class
+ * @property id $id
+ * @property string|null $name
+ * @property string|null $dummy
+ */
 
 #[ORM\Database(name: 'mysql')]
 #[ORM\Table(name: 'user')]
 class User extends Model
 {
-    protected $test;
+    #[ORM\Column(name: 'id', type: 'bigint', primary: true, autoIncrement: true, index: true, unique: true)]
+    protected int $id;
 
-    public function __construct()
-    {
-        dump(self::getDatabase());
-    }
+    #[ORM\Column(name: 'name', type: 'varchar', length: 256, nullable: true)]
+    protected string|null $name = null;
+
+    #[ORM\Column(name: 'dummy', type: 'varchar', length: 256, default: 'dummy')]
+    protected string|null $dummy = 'dummy';
 }
