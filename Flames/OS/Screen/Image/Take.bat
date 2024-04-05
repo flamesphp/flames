6d68c1ec6d7c8ed9bb70d0253934400b156cb854
Gabriel 'Kazz' Morgado <kazzxd1@gmail.com>
2024-04-04 23:10:37 -0300

    PSR

--- Flames/OS/Screen/Image/Take.bat
+++ Flames/OS/Screen/Image/Take.bat
@@ -23,9 +23,6 @@ endlocal & exit /b %errorlevel%
 
 */
 
-// reference  
-// https://gallery.technet.microsoft.com/scriptcenter/eeff544a-f690-4f6b-a586-11eea6fc5eb8
-
 using System;
 using System.Runtime.InteropServices;
 using System.Drawing;
@@ -33,54 +30,33 @@ using System.Drawing.Imaging;
 using System.Collections.Generic;
 using Microsoft.VisualBasic;
 
-
-/// Provides functions to capture the entire screen, or a particular window, and save it to a file. 
-
 public class ScreenCapture
 {
-
-    /// Creates an Image object containing a screen shot the active window 
-
     public Image CaptureActiveWindow()
     {
         return CaptureWindow(User32.GetForegroundWindow());
     }
-
-    /// Creates an Image object containing a screen shot of the entire desktop 
 
     public Image CaptureScreen()
     {
         return CaptureWindow(User32.GetDesktopWindow());
     }
 
-    /// Creates an Image object containing a screen shot of a specific window 
-
     private Image CaptureWindow(IntPtr handle)
     {
-        // get te hDC of the target window 
         IntPtr hdcSrc = User32.GetWindowDC(handle);
-        // get the size 
         User32.RECT windowRect = new User32.RECT();
         User32.GetWindowRect(handle, ref windowRect);
         int width = windowRect.right - windowRect.left;
         int height = windowRect.bottom - windowRect.top;
-        // create a device context we can copy to 
         IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
-        // create a bitmap we can copy it to, 
-        // using GetDeviceCaps to get the width/height 
         IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
-        // select the bitmap object 
         IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
-        // bitblt over 
         GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
-        // restore selection 
         GDI32.SelectObject(hdcDest, hOld);
-        // clean up 
         GDI32.DeleteDC(hdcDest);
         User32.ReleaseDC(handle, hdcSrc);
-        // get a .NET image object for it 
         Image img = Image.FromHbitmap(hBitmap);
-        // free up the Bitmap object 
         GDI32.DeleteObject(hBitmap);
         return img;
     }
@@ -105,13 +81,12 @@ public class ScreenCapture
     static void parseArguments()
     {
         String[] arguments = Environment.GetCommandLineArgs();
-        if (arguments.Length == 1)
-        {
+        if (arguments.Length == 1) {
             printHelp();
             Environment.Exit(0);
         }
-        if (arguments[1].ToLower().Equals("/h") || arguments[1].ToLower().Equals("/help"))
-        {
+
+        if (arguments[1].ToLower().Equals("/h") || arguments[1].ToLower().Equals("/help")) {
             printHelp();
             Environment.Exit(0);
         }
@@ -130,32 +105,24 @@ public class ScreenCapture
         formats.Add("tiff", System.Drawing.Imaging.ImageFormat.Tiff);
         formats.Add("wmf", System.Drawing.Imaging.ImageFormat.Wmf);
 
-
         String ext = "";
-        if (file.LastIndexOf('.') > -1)
-        {
+        if (file.LastIndexOf('.') > -1) {
             ext = file.ToLower().Substring(file.LastIndexOf('.') + 1, file.Length - file.LastIndexOf('.') - 1);
-        }
-        else
-        {
+        }  else {
             Console.WriteLine("Invalid file name - no extension");
             Environment.Exit(7);
         }
 
-        try
-        {
+        try  {
             format = formats[ext];
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             Console.WriteLine("Probably wrong file format:" + ext);
             Console.WriteLine(e.ToString());
             Environment.Exit(8);
         }
 
-
-        if (arguments.Length > 2)
-        {
+        if (arguments.Length > 2) {
             windowTitle = arguments[2];
             fullscreen = false;
         }
@@ -164,20 +131,8 @@ public class ScreenCapture
 
     static void printHelp()
     {
-        //clears the extension from the script name
         String scriptName = Environment.GetCommandLineArgs()[0];
         scriptName = scriptName.Substring(0, scriptName.Length);
-        Console.WriteLine(scriptName + " captures the screen or the active window and saves it to a file.");
-        Console.WriteLine("");
-        Console.WriteLine("Usage:");
-        Console.WriteLine(" " + scriptName + " filename  [WindowTitle]");
-        Console.WriteLine("");
-        Console.WriteLine("filename - the file where the screen capture will be saved");
-        Console.WriteLine("     allowed file extensions are - Bmp,Emf,Exif,Gif,Icon,Jpeg,Png,Tiff,Wmf.");
-        Console.WriteLine("WindowTitle - instead of capture whole screen you can point to a window ");
-        Console.WriteLine("     with a title which will put on focus and captuted.");
-        Console.WriteLine("     For WindowTitle you can pass only the first few characters.");
-        Console.WriteLine("     If don't want to change the current active window pass only \"\"");
     }
 
     public static void Main()
@@ -188,47 +143,30 @@ public class ScreenCapture
         ScreenCapture sc = new ScreenCapture();
         if (!fullscreen && !windowTitle.Equals(""))
         {
-            try
-            {
-
+            try {
                 Interaction.AppActivate(windowTitle);
-                Console.WriteLine("setting " + windowTitle + " on focus");
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Probably there's no window like " + windowTitle);
-                Console.WriteLine(e.ToString());
+            } catch (Exception e) {
                 Environment.Exit(9);
             }
-
-
         }
-        try
-        {
-            if (fullscreen)
-            {
-                Console.WriteLine("Taking a capture of the whole screen to " + file);
+
+        try {
+            if (fullscreen) {
                 sc.CaptureScreenToFile(file, format);
             }
-            else
-            {
-                Console.WriteLine("Taking a capture of the active window to " + file);
+            else {
                 sc.CaptureActiveWindowToFile(file, format);
             }
         }
-        catch (Exception e)
-        {
-            Console.WriteLine("Check if file path is valid " + file);
+        catch (Exception e) {
             Console.WriteLine(e.ToString());
         }
     }
 
-    /// Helper class containing Gdi32 API functions 
-
     private class GDI32
     {
 
-        public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter 
+        public const int SRCCOPY = 0x00CC0020;
         [DllImport("gdi32.dll")]
         public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
           int nWidth, int nHeight, IntPtr hObjectSource,
@@ -246,14 +184,10 @@ public class ScreenCapture
         public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
     }
 
-
-    /// Helper class containing User32 API functions 
-
     private class User32
     {
         [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
+        public struct RECT {
             public int left;
             public int top;
             public int right;
