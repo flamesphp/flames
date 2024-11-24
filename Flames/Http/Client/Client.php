401f87b871bace3f050b089e1cc0c83db4781858
Gabriel Morgado <kazzxd1@gmail.com>
2024-11-24 00:55:58 -0300

    Client Http

--- Flames/Http/Client/Client.php
+++ Flames/Http/Client/Client.php
@@ -13,9 +13,9 @@ use Flames\Http\Async\Response;
  */
 class Client
 {
-    protected static $lastId = 0;
+//    protected static $lastId = 0;
 
-    protected static $callbacks = [];
+//    protected static $callbacks = [];
 
     protected string|null $baseUri = null;
     protected Request|null $request = null;
@@ -32,31 +32,64 @@ class Client
     public function sendAsync(Request $request)
     {
         $this->request = $request;
-
         return $this;
     }
 
     public function then(\Closure $delegate)
     {
-        self::$lastId += 1;
-        self::$callbacks[self::$lastId] = $delegate;
+        $window = Js::getWindow();
+        $method = strtolower($this->request->getMethod());
 
-        $data = [
-            'id'      => self::$lastId,
-            'method'  => strtolower($this->request->getMethod()),
-            'url'     => $this->request->getUri(),
-            'header'  => $this->request->getHeaders()
-        ];
-
+        $uri = $this->request->getUri();
         if ($this->baseUri !== null) {
-            $data['url'] = ($this->baseUri . $data['url']);
+            $uri = ($this->baseUri . $uri);
         }
 
-        if (isset($data['header']['body']) === true) {
-            $data['header']['body'] = base64_encode($data['header']['body']);
+        $headers = $this->request->getHeaders();
+
+        $xmlHttpRequest = new $window->XMLHttpRequest();
+        $xmlHttpRequest->onreadystatechange = function () use ($xmlHttpRequest, $window, $delegate) {
+            if ($xmlHttpRequest->readyState == $window->XMLHttpRequest->DONE) {
+                $code = (int)$xmlHttpRequest->status;
+                $body = $xmlHttpRequest->responseText;
+                $headers = [];
+                $headersLines = explode("\n", $xmlHttpRequest->getAllResponseHeaders());
+                foreach ($headersLines as $headerLine) {
+                    $firstDouble = strpos($headerLine, ':');
+                    if ($firstDouble === false) {
+                        continue;
+                    }
+                    $key = trim(substr($headerLine, 0, $firstDouble));
+                    $value = trim(substr($headerLine, $firstDouble + 1));
+                    $headers[$key] = $value;
+                }
+
+                $response = new Response($code, $body, $headers);
+                $delegate($response);
+            }
+        };
+        $xmlHttpRequest->open($method, $uri);
+
+        foreach ($headers as $header => $content) {
+            if ($header === 'body') {
+                continue;
+            }
+            $xmlHttpRequest->setRequestHeader($header, $content);
         }
 
-        Js::eval('Flames.Internal.Http(\'' . serialize($data) . '\');');
+        if ($method !== 'get') {
+            $body = null;
+            if (isset($headers['body']) === true) {
+                $body = $headers['body'];
+            }
+
+            if ($body !== null) {
+                $xmlHttpRequest->send($body);
+                return;
+            }
+        }
+
+        $xmlHttpRequest->send();
     }
 
 
@@ -85,46 +118,46 @@ class Client
         throw new Exception('Function unsupported on client side, use request.');
     }
 
-    public static function callback(int $id, string $status, string $body, string $header = null)
-    {
-        $delegate = self::$callbacks[$id];
-
-        $data = (object)[
-            'status' => base64_decode($status),
-            'message' => null,
-            'body' => null,
-            'header' => null
-        ];
-
-        if ($data->status !== 'error') {
-            $data->body = base64_decode($body);
-            $data->header = json_decode(base64_decode($header));
-        } else {
-            $data->message = base64_decode($body);
-        }
-
-        $body = null;
-        $code = 200;
-        $headers = null;
-        if ($data->status === 'error') {
-            $code = 500;
-            $body = $data->message;
-        } else {
-            $code = $data->status;
-            if (is_object($data->body) === true || is_array($data->body) === true) {
-                $data->body = json_encode($data->body);
-            }
-            $body = $data->body;
-        }
-
-        $_header = Arr();
-        if ($data->header !== null) {
-            foreach ($data->header as $header) {
-                $_header[$header[0]] = $header[1];
-            }
-        }
-
-        $response = new Response($code, $body, $_header);
-        $delegate($response);
-    }
+//    public static function callback(int $id, string $status, string $body, string $header = null)
+//    {
+//        $delegate = self::$callbacks[$id];
+//
+//        $data = (object)[
+//            'status' => base64_decode($status),
+//            'message' => null,
+//            'body' => null,
+//            'header' => null
+//        ];
+//
+//        if ($data->status !== 'error') {
+//            $data->body = base64_decode($body);
+//            $data->header = json_decode(base64_decode($header));
+//        } else {
+//            $data->message = base64_decode($body);
+//        }
+//
+//        $body = null;
+//        $code = 200;
+//        $headers = null;
+//        if ($data->status === 'error') {
+//            $code = 500;
+//            $body = $data->message;
+//        } else {
+//            $code = $data->status;
+//            if (is_object($data->body) === true || is_array($data->body) === true) {
+//                $data->body = json_encode($data->body);
+//            }
+//            $body = $data->body;
+//        }
+//
+//        $_header = Arr();
+//        if ($data->header !== null) {
+//            foreach ($data->header as $header) {
+//                $_header[$header[0]] = $header[1];
+//            }
+//        }
+//
+//        $response = new Response($code, $body, $_header);
+//        $delegate($response);
+//    }
 }
